129417b8009650593eab6ce36eb5ab52ebadb993
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-23 15:48:30 +0100

    secondo commit

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -1,10 +1,25 @@
 package org.generation.italy.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Inserisci la base e l'altezza: ");
+		
+			
+		int base = scan.nextInt();
+		int altezza = scan.nextInt(); 
+		
+		
+		Rettangolo R = new Rettangolo (base, altezza);
+		int a = R.area();
+		System.out.println("L'area è: " + a);
+		int p = R.perimetro();
+		System.out.println("Il perimetro è: " + p);
+				
+		scan.close();
 	}
 
 }
--- src/org/generation/italy/geometria/Rettangolo.java
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -2,4 +2,30 @@ package org.generation.italy.geometria;
 
 public class Rettangolo {
 
+	//attributi
+	
+	int base;
+	int altezza;
+	
+	//costruttore
+	
+	Rettangolo(int base, int altezza){
+		this.base = base;
+		this.altezza = altezza;
+	}
+	
+	//metodi
+	
+	//calcola area
+	int area () {		
+		return base * altezza;
+	}
+	
+	
+	//calcolo perimetro
+	int perimetro () {		
+		return ((base *2) + (altezza*2));		
+	}
+	
+	
 }
